01892c9e9407e43836b5613309addef77d55ab3c
Sumit Samanta <sumit.samanta@jungleworks.co>
2018-04-30 14:20:26 +0530

    add compression module changes

--- server.js
+++ server.js
@@ -6,12 +6,14 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
 
+// compress all responses
+app.use(compression());
+
 app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
-// compress all responses
-app.use(compression());
+
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
